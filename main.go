091b94ee47f013177c0b69fc4d9e808f3fa42c54
Guilhem Lettron <guilhem@barpilot.io>
2018-05-23 00:03:51 +0200

    hum...

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
+	"github.com/containers/image/copy"
 	"github.com/containers/image/docker"
+	"github.com/containers/image/docker/archive"
 	"github.com/containers/image/docker/reference"
-	"github.com/containers/image/docker/tarfile"
+	"github.com/containers/image/signature"
+	"github.com/containers/image/types"
 )
 
 func main() {
@@ -36,7 +40,7 @@ func HandleClient(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	img, err := ref.NewImage(ctx, nil)
+	//img, err := ref.NewImage(ctx, nil)
 	srcimg, err := ref.NewImageSource(ctx, nil)
 	if err != nil {
 		http.Error(writer, "Fail to parse 'image'", 400)
@@ -46,26 +50,61 @@ func HandleClient(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Client requests: " + image)
 
 	//refdest := reference.NamedTagged{}
-	refTagged, err := reference.WithTag(ref.DockerReference(), "latest")
-	dest := tarfile.NewDestination(writer, refTagged)
+	//refTagged, err := reference.WithTag(ref.DockerReference(), "latest")
+	refTagged, ok := ref.DockerReference().(reference.NamedTagged)
+	if !ok {
+		http.Error(writer, "Fail to get ref", 400)
+		return
+	}
+	dest, err := archive.NewImageDestinationWriter(nil, refTagged, writer)
+	if err != nil {
+		http.Error(writer, "Fail NewImageDestinationWriter", 400)
+		return
+	}
+
+	policy := &signature.Policy{Default: []signature.PolicyRequirement{signature.NewPRInsecureAcceptAnything()}}
+
+	sctx, err := signature.NewPolicyContext(policy)
+	if err != nil {
+		http.Error(writer, "Fail NewPolicyContext", 400)
+		return
+	}
+
+	reportWriter := os.Stdout
+	if err := copy.ImageR(ctx, sctx, dest, srcimg, &copy.Options{
+		RemoveSignatures: false,
+		SignBy:           "",
+		ReportWriter:     reportWriter,
+		SourceCtx:        nil, //s.systemContext,
+		DestinationCtx:   &types.SystemContext{},
+	}); err != nil {
+		http.Error(writer, "Fail to get source information, err", 400)
+		return
+	}
 
 	//Send the headers
 	//writer.Header().Set("Content-Type", FileContentType)
 
-	blobs := img.LayerInfos()
-	if err != nil || len(blobs) == 0 {
-		http.Error(writer, "Fail to get source information", 400)
-		return
-	}
-
-	reader, _, err := srcimg.GetBlob(ctx, blobs[0])
-	if err != nil {
-		http.Error(writer, "Fail to get source data", 400)
-		return
-	}
-
+	// blobs := img.LayerInfos()
+	// img.ConfigInfo()
+	// if err != nil || len(blobs) == 0 {
+	// 	http.Error(writer, "Fail to get source information", 400)
+	// 	return
+	// }
 	writer.Header().Set("Content-Disposition", "attachment; filename="+image+".tar")
-	//writer.Header().Set("Content-Length", size)
-	dest.PutBlob(ctx, reader, blobs[0], false)
+
+	// manifest, _, err := srcimg.GetManifest(ctx, nil)
+	// dest.PutManifest(ctx, manifest)
+	// for _, blob := range blobs {
+	// 	reader, _, err := srcimg.GetBlob(ctx, blob)
+	// 	if err != nil {
+	// 		http.Error(writer, "Fail to get source data", 400)
+	// 		return
+	// 	}
+	// 	defer reader.Close()
+	//
+	// 	//writer.Header().Set("Content-Length", size)
+	// 	dest.PutBlob(ctx, reader, blob, false)
+	// }
 	return
 }
